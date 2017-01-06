f0b4e823d63795738c3fc0a91c26a6c4fc3d757b
Alisher Sadikov <alishersadikov@gmail.com>
2017-01-06 11:20:59 -0700

    build api index

--- spec/requests/api/v1/storedom_internal_api_spec.rb
+++ spec/requests/api/v1/storedom_internal_api_spec.rb
@@ -2,16 +2,54 @@ require 'rails_helper'
 
 describe "storedom endpoints" do
   context "GET /api/v1/items" do
-    it "returns a list of all destinations" do
-      
+    it "returns a list of all items" do
+      item_1 = Item.create!(
+        name: Faker::Commerce.product_name,
+        description: Faker::Lorem.paragraph,
+        image_url: "http://robohash.org/1.png?set=set2&bgset=bg1&size=200x200"
+        )
+
+      item_2 = Item.create!(
+        name: Faker::Commerce.product_name,
+        description: Faker::Lorem.paragraph,
+        image_url: "http://robohash.org/2.png?set=set2&bgset=bg1&size=200x200"
+        )
+
       get "/api/v1/items"
 
       items = JSON.parse(response.body, symbolize_names: true)
 
       expect(response).to be_success
-      expect(destinations.count).to eq(2)
-      expect(destinations.first[:name]).to eq(destination_1.name)
-      expect(destinations.last[:name]).to eq(destination_2.name)
+      expect(items).to be_an(Array)
+      expect(items.count).to eq(2)
+      expect(items.first[:name]).to eq(item_1.name)
+      expect(items.last[:name]).to eq(item_2.name)
+    end
+  end
+
+  context "GET /api/v1/items" do
+    it "returns a list of all items" do
+      item_1 = Item.create!(
+        name: Faker::Commerce.product_name,
+        description: Faker::Lorem.paragraph,
+        image_url: "http://robohash.org/1.png?set=set2&bgset=bg1&size=200x200"
+        )
+
+      item_2 = Item.create!(
+        name: Faker::Commerce.product_name,
+        description: Faker::Lorem.paragraph,
+        image_url: "http://robohash.org/2.png?set=set2&bgset=bg1&size=200x200"
+        )
+
+      get "/api/v1/items"
+
+      items = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to be_success
+      expect(items).to be_an(Array)
+      expect(items.count).to eq(2)
+      expect(items.first[:name]).to eq(item_1.name)
+      expect(items.last[:name]).to eq(item_2.name)
     end
   end
 end
