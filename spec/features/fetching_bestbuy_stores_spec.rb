62d88099196ab18320f7969ccedb5ee739127630
Alisher Sadikov <alishersadikov@gmail.com>
2017-01-06 10:42:29 -0700

    pass first feature test

--- spec/features/fetching_bestbuy_stores_spec.rb
+++ spec/features/fetching_bestbuy_stores_spec.rb
@@ -1,25 +1,44 @@
 require 'rails_helper'
 
 feature "user visits root path" do
-  VCR.use_cassette("storedom") do
-    it "they see stuff" do
-        #       As a user
-        # When I visit "/"
-        # And I fill in a search box with "80202" and click "search"
-        # Then my current path should be "/search" (ignoring params)
-        # And I should see stores within 25 miles of 80202
-        # And I should see a message that says "16 Total Stores"
-        # And I should see exactly 15 results
-        # And I should see the long name, city, distance, phone number and store type for each of the 15 results
+  it "they search by zipcode" do
+      VCR.use_cassette("storedom") do
       visit "/"
-      save_and_open_page
-      fill_in '#search-field', with: "80202"
-      # fill_in '#search-field', with: "80203"
 
+      fill_in 'zipcode', with: "80202"
+      click_on "Search"
 
-      # click "Search"
-      # expect(current_path).to eq(search_path)
-      # expect(page).to have_content("16 Total Stores")
+      expect(current_path).to eq(search_path)
+      expect(page).to have_content("16 Total Stores")
+      expect(page).to have_content("Long Name")
+      expect(page).to have_content("City")
+      expect(page).to have_content("Phone Number")
+      expect(page).to have_content("Distance")
+      expect(page).to have_content("Store Type")
+
+      expect(page).to have_content("Best Buy Mobile - Cherry Creek Shopping Center")
+      expect(page).to have_content("Denver")
+
+      expect(page).to have_content("Best Buy - Parker")
+      expect(page).to have_content("303-840-5501")
     end
+  end
+
+  it "they see store hours" do
+
+
+#   As a user
+# After I have searched a zip code for stores
+# When I click the name of a store
+# Then my current path should be "/stores/:store_id"
+# I should see the store name, store type and address with city, state and zip
+# I should see an unordered list of the store hours in the following format:
+#   * Mon: 10am-9pm
+#   * Tue: 10am-9pm
+#   * Wed: 10am-9pm
+#   * Thurs: 10am-9pm
+#   * Fri: 10am-9pm
+#   * Sat: 10am-9pm
+#   * Sun: 11am-7pm
   end
 end
