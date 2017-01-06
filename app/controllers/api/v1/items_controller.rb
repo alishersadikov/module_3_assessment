685c30653e5d0be58e3affb5c10e4edbb621e8bc
Alisher Sadikov <alishersadikov@gmail.com>
2017-01-06 11:28:17 -0700

    build api show

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -2,4 +2,13 @@ class Api::V1::ItemsController < ApplicationController
   def index
     render json: Item.all
   end
+
+  def show
+    render json: Item.find(params[:id])
+  end
+
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+  end
 end
--- spec/requests/api/v1/storedom_internal_api_spec.rb
+++ spec/requests/api/v1/storedom_internal_api_spec.rb
@@ -27,8 +27,8 @@ describe "storedom endpoints" do
     end
   end
 
-  context "GET /api/v1/items" do
-    it "returns a list of all items" do
+  context "GET /api/v1/items/1" do
+    it "returns a specific items" do
       item_1 = Item.create!(
         name: Faker::Commerce.product_name,
         description: Faker::Lorem.paragraph,
@@ -41,15 +41,28 @@ describe "storedom endpoints" do
         image_url: "http://robohash.org/2.png?set=set2&bgset=bg1&size=200x200"
         )
 
-      get "/api/v1/items"
+      get "/api/v1/items/1"
 
-      items = JSON.parse(response.body, symbolize_names: true)
+      item = JSON.parse(response.body, symbolize_names: true)
 
       expect(response).to be_success
-      expect(items).to be_an(Array)
-      expect(items.count).to eq(2)
-      expect(items.first[:name]).to eq(item_1.name)
-      expect(items.last[:name]).to eq(item_2.name)
+      expect(item[:name]).to eq(item_1.name)
+      expect(item[:description]).to eq(item_1.description)
+    end
+  end
+
+  context "DELETE /api/v1/items/1" do
+    it "returns a specific items" do
+      item_1 = Item.create!(
+        name: Faker::Commerce.product_name,
+        description: Faker::Lorem.paragraph,
+        image_url: "http://robohash.org/1.png?set=set2&bgset=bg1&size=200x200"
+        )
+
+      delete "/api/v1/items/item_1.id"
+
+      expect(response).to be_success
+      expect(Item.all).to eq([])
     end
   end
 end
