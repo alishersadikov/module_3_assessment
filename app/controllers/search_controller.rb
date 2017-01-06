72e44c2a39d32e7d30ace0d518c7d1d0d67c88fc
Alisher Sadikov <alishersadikov@gmail.com>
2017-01-06 09:54:05 -0700

    create Store class

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,5 +3,13 @@ class SearchController < ApplicationController
     zipcode = params["zipcode"]
     response = Faraday.get("https://api.bestbuy.com/v1/stores(area(#{zipcode},25))?format=json&show=storeId,storeType,name,city,region&apiKey=445jt4mb4gsenju48n2ndu8d&pageSize=25")
     parsed = JSON.parse(response.body, symbolize_names: true)
+
+    Store.new.load_stores("parsed")
+  end
+end
+
+class Store
+  def load_stores(parsed)
+    binding.pry
   end
 end
