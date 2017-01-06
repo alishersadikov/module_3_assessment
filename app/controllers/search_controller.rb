01de2af0bfc3665d2d5d9c43a439e339fd1d2cc3
Alisher Sadikov <alishersadikov@gmail.com>
2017-01-06 10:10:43 -0700

    build search view

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,23 +1,9 @@
 class SearchController < ApplicationController
   def index
-    zipcode = params["zipcode"]
-    response = Faraday.get("https://api.bestbuy.com/v1/stores(area(#{zipcode},25))?format=json&show=longName,city,distance,phone,storeType&apiKey=445jt4mb4gsenju48n2ndu8d&pageSize=25")
+    @zipcode = params["zipcode"]
+    response = Faraday.get("https://api.bestbuy.com/v1/stores(area(#{@zipcode},25))?format=json&show=longName,city,distance,phone,storeType&apiKey=445jt4mb4gsenju48n2ndu8d&pageSize=25")
     parsed = JSON.parse(response.body, symbolize_names: true)
 
-    Store.load_stores(parsed)
-  end
-end
-
-class Store
-  def initialize(store_data)
-    name = store_data[:longName]
-    city = store_data[:city]
-    distance = store_data[:distance]
-    phone = store_data[:phone]
-    type = store_data[:storeType]
-  end
-
-  def self.load_stores(parsed)
-    parsed[:stores].map { |raw_store|  Store.new(raw_store) }
+    @stores = Store.load_stores(parsed)
   end
 end
---
+++ app/models/store.rb
@@ -0,0 +1,15 @@
+class Store
+  attr_reader :name, :city, :distance, :phone, :type
+
+  def initialize(store_data)
+    @name = store_data[:longName]
+    @city = store_data[:city]
+    @distance = store_data[:distance]
+    @phone = store_data[:phone]
+    @type = store_data[:storeType]
+  end
+
+  def self.load_stores(parsed)
+    parsed[:stores].map { |raw_store|  Store.new(raw_store) }
+  end
+end
